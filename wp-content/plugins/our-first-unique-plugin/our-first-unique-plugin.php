39806ac26650c51c5cde5538fa1c67eefc8727bd
newton.varun <newton.varun@rubicotech.in>
2024-04-04 13:13:59 +0530

    implemented the settings api and made the UI on admin dashboard

--- wp-content/plugins/our-first-unique-plugin/our-first-unique-plugin.php
+++ wp-content/plugins/our-first-unique-plugin/our-first-unique-plugin.php
@@ -7,11 +7,152 @@ Author: Varun
 Author URI:
 */
 
-add_filter("the_content", "addToEndOfPost");
+class WordCountAndTimePlugin
+{
+  //Constructor will get invoked as we instantiate the class
+  function __construct()
+  {
+    //adminPage function will be call on this object So no chances of conflict.
+    add_action('admin_menu', array($this, 'adminPage'));
 
-function addToEndOfPost($content){
-  if(is_single() && is_main_query()){
-  return $content . '<p>My name is varun</p>';
+    //Calling the setting function on admin hook
+    add_action('admin_init', array($this, 'settings'));
+
+    add_filter('the_content', array($this, 'ifWrap'));
   }
-  return $content;
+
+  function ifWrap($content)
+  {
+    if (
+      ((is_main_query() and is_single()) and (
+        get_option('wcp_wordcount', '1') or //using 1 as second parameter is the default value when plugin gets installed first time and it did'nt exist in db.
+        get_option('wcp_charactercount', '1') or
+        get_option('wcp_readtime', '1')))
+    ) {
+      return $this->createHTML($content);
+    }
+    return $content;
+  }
+
+  function createHTML($content)
+  {
+    $html = '<h3>' . get_option('wcp_headline', 'Post Statistics') . '</h3> <p>';
+    if (get_option('wcp_location', '0') == '0') {
+      return $html . $content;
+    } else {
+      return $content . $html;
+    }
+  }
+
+  // Function for registering section and settings
+  function settings()
+  {
+    //Adding the setting section.
+    add_settings_section('wcp_first_section', null, null, 'word-count-settings-page');
+
+    // Adding the setting for the location field .
+    add_settings_field('wcp_location', 'Display Location', array($this, 'locationHTML'), 'word-count-settings-page', 'wcp_first_section');
+    // Registering the setting of location field
+    register_setting('wordcountplugin', 'wcp_location', array('sanitize_callback' => array($this, 'sanitizeLocation'), 'default' => '0'));
+
+    // Adding the setting for the headline field .
+    add_settings_field('wcp_headline', 'Display Headline', array($this, 'headlineHTML'), 'word-count-settings-page', 'wcp_first_section');
+    // Registering the setting of headline field
+    register_setting('wordcountplugin', 'wcp_headline', array('sanitize_callback' => 'sanitize_text_field', 'default' => 'Post Statistics'));
+
+    // Adding the setting for the word count field .
+    add_settings_field('wcp_wordcount', 'Word Count', array($this, 'checkboxHTML'), 'word-count-settings-page', 'wcp_first_section', array('theName' => 'wcp_wordcount'));
+    // Registering the setting of word count field
+    register_setting('wordcountplugin', 'wcp_wordcount', array('sanitize_callback' => 'sanitize_text_field', 'default' => '1'));
+
+    // Adding the setting for the character count field .
+    add_settings_field('wcp_charactercount', 'Character Count', array($this, 'checkboxHTML'), 'word-count-settings-page', 'wcp_first_section', array('theName' => 'wcp_charactercount'));
+    // Registering the setting of character count field
+    register_setting('wordcountplugin', 'wcp_charactercount', array('sanitize_callback' => 'sanitize_text_field', 'default' => '1'));
+
+    // Adding the setting for the read time field .
+    add_settings_field('wcp_readtime', 'Read Time', array($this, 'checkboxHTML'), 'word-count-settings-page', 'wcp_first_section', array('theName' => 'wcp_readtime'));
+    // Registering the setting of read time field
+    register_setting('wordcountplugin', 'wcp_readtime', array('sanitize_callback' => 'sanitize_text_field', 'default' => '1'));
+  }
+
+  // Function to render the html of readtime field
+  /*
+  Thre different function for each check box.
+  function readtimeHTML()
+  { ?>
+    <input type="checkbox" name="wcp_readtime" value="1" <?php checked(get_option('wcp_readtime', '1')) ?>>
+  <?php }
+
+  // Function to render the html of charactercount field
+  function charactercountHTML()
+  { ?>
+    <input type="checkbox" name="wcp_charactercount" value="1" <?php checked(get_option('wcp_charactercount', '1')) ?>>
+  <?php }
+
+  // Function to render the html of wordcount field
+  function wordcountHTML()
+  { ?>
+    <input type="checkbox" name="wcp_wordcount" value="1" <?php checked(get_option('wcp_wordcount', '1')) ?>>
+  <?php }
+  */
+
+//Sanitize the value so that user does'nt input malicious value from dev tools.
+  function sanitizeLocation($input)
+  {
+    if ($input !== '0' and $input !== '1') {
+      add_settings_error('wcp_location', 'wcp_location_error', 'Display Location must be either beginnening or end');
+      return get_option('wcp_location');
+    }
+    return $input;
+  }
+
+  // Single function to be called on all three checkboxes 
+  function checkboxHTML($args)
+  {
+    ?>
+    <input type="checkbox" name="<?php echo $args['theName']; ?>" value="1" <?php checked(get_option($args['theName'], '1')) ?>>
+  <?php }
+  // Function to render the html of headline field
+  function headlineHTML()
+  { ?>
+    <input type="text" name='wcp_headline' value="<?php echo esc_attr(get_option('wcp_headline')) ?>">
+  <?php }
+
+  // Function to render the html of location field
+  function locationHTML()
+  { ?>
+    <!-- name attribute should match with the register setting arguement name -->
+    <select name="wcp_location">
+      <option value="0" <?php selected(get_option('wcp_location'), '0'); ?>>Beginning of post</option>
+      <option value="1" <?php selected(get_option('wcp_location'), '1'); ?>>End of post</option>
+    </select>
+  <?php }
+  function adminPage()
+  {
+    add_options_page(
+      'Word Count Settings',
+      'Word Count',
+      'manage_options',
+      'word-count-settings-page',
+      array($this, 'ourHTML')
+    );
+  }
+  //Function that will render the UI of the plugin on dashboard.
+  function ourHTML()
+  { ?>
+    <div class="wrap">
+      <h1>Word Count Settings</h1>
+      <form action="options.php" method="POST">
+        <?php
+        settings_fields('wordcountplugin');
+        do_settings_sections('word-count-settings-page');
+        submit_button();
+        ?>
+      </form>
+    </div>
+  <?php }
 }
+//Instantiation of the class.
+$WordCountAndTimePlugin = new WordCountAndTimePlugin();
+
